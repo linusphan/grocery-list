fd4d5aa13f7c755e478ac76ccebee60ca6e3eae8
Linus Phan <phanlinus@gmail.com>
2019-06-30 23:29:52 -0700

    add features

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,11 +1,23 @@
+const template = Handlebars.compile($('#items').html());
+Handlebars.registerPartial('item', $('#item').html());
+
 const ItemModel = Backbone.Model.extend({
+  idAttribute: 'id',
 });
 
 const items = {
+  $body: $('tbody'),
   collection: [],
 
+  empty: function () {
+    this.collection = [];
+    this.render();
+  },
+
   create: function (itemData) {
+    itemData.id = this.collection.length + 1;
     const item = new ItemModel(itemData);
+
     this.collection.push(item);
 
     return item;
@@ -16,12 +28,62 @@ const items = {
   },
 
   render: function () {
-    
+    this.$body.html(template({ items: this.collection }));
+  },
+
+  remove: function (e) {
+    e.preventDefault();
+
+    const $e = $(e.currentTarget);
+    const model = _(this.collection).findWhere({ id: +$e.attr('data-id') });
+
+    this.collection = _(this.collection).without(model);
+    this.render();
+  },
+
+  sortBy: function (prop) {
+    this.collection = _(this.collection).sortBy(function (model) {
+      return model.attributes[prop];
+    });
+
+    this.render();
+  },
+
+  bind: function () {
+    this.$body.on('click', 'a', this.remove.bind(this));
+  },
+
+  init: function () {
+    this.seedCollection();
+    this.render();
+    this.bind();
   },
 };
 
-const template = Handlebars.compile($('#items').html());
+items.init();
 
-Handlebars.registerPartial('item', $('#item').html());
+$('form').on('submit', function (e) {
+  e.preventDefault();
 
-items.seedCollection();
+  const inputs = $(this).serializeArray();
+  const attrs = {};
+
+  inputs.forEach(function (input) {
+    attrs[input.name] = input.value;
+  });
+
+  const item = items.create(attrs);
+  items.$body.append(Handlebars.partials.item(item.toJSON()));
+  this.reset();
+});
+
+$('th').on('click', function () {
+  const prop = $(this).attr('data-prop');
+  items.sortBy(prop);
+});
+
+$('p a').on('click', function (e) {
+  e.preventDefault();
+
+  items.empty();
+});
